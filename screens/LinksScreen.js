64540774bc2d0a4cb201c755f84a5ff4c8d8ce9f
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-12-11 17:47:38 -0500

    mapComponent, react-native-maps

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { Content, Container, Header, Title, Button, Left, Right, Body, Icon, Tab, Tabs, ScrollableTab } from 'native-base';
+import MapView, { Marker, Circle, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
-  };
-
+    header: null,
+  }
   render() {
+
+    const MapComponent = () =>
+      <Container style={{ maxHeight:"100%"}}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={ {height:"100%"} }
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+      </Container>
+
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+      <ScrollView>
+        <MapComponent/>
       </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
 });
