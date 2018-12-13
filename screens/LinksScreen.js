37ebfee26be7b2fd8c655b21c58933eff71da4f0
americanman <snowboardracer7@gmail.com>
2018-12-12 20:42:12 -0500

    Refactore Map

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
-import { Content, Container, Header, Title, Button, Left, Right, Body, Icon, Tab, Tabs, ScrollableTab } from 'native-base';
-import MapView, { Marker, Circle, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import Map from './../components/Map.js'
 
 
 export default class LinksScreen extends React.Component {
@@ -10,25 +8,11 @@ export default class LinksScreen extends React.Component {
     header: null,
   }
   render() {
-
-    const MapComponent = () =>
-      <Container style={{ maxHeight:"100%"}}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={ {height:"100%"} }
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
-      </Container>
-
     return (
       <ScrollView>
-        <MapComponent/>
+        <Map latitude={37.78825}
+             longitude={-122.4324}
+        />
       </ScrollView>
     );
   }
